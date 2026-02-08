96da8f7d46c47f0225ecb01d998f895297fd52ee
ayannapeterson5 <aypeters@syr.edu>
2026-02-08 17:28:16 +0000

    final3

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit as st
 from pathlib import Path
 
 st.set_page_config(page_title="IST488 Homework", layout="centered")
+st.title("Homework Manager")
 
-choice = st.sidebar.radio("Select a page", ["HW 1", "HW 2", "HW 3"])
+choice = st.sidebar.radio("Select assignment", ["HW 1", "HW 2", "HW 3"])
 
 if choice == "HW 1":
     exec(Path("HW/HW1.py").read_text(), {})
@@ -11,6 +12,7 @@ elif choice == "HW 2":
     exec(Path("HW/HW2.py").read_text(), {})
 else:
     exec(Path("HW/HW3.py").read_text(), {})
+
 
 
 
