eb1b94eadecfd07d8b8295474195cd9450ef337e
ayannapeterson5 <aypeters@syr.edu>
2026-02-09 02:56:18 +0000

    Add beautifulsoup4 dependency

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,4 @@
-import streamlit as st
-from pathlib import Path
-
-st.set_page_config(page_title="IST 488 Homework", layout="centered")
-st.title("Homework Manager")
-
-choice = st.sidebar.radio(
-    "Select assignment",
-    ["HW 1", "HW 2", "HW 3"]
-)
+choice = st.sidebar.radio("Select assignment", ["HW 1", "HW 2", "HW 3"])
 
 if choice == "HW 1":
     exec(Path("HW/HW1.py").read_text(), {})
@@ -15,6 +6,7 @@ elif choice == "HW 2":
     exec(Path("HW/HW2.py").read_text(), {})
 else:
     exec(Path("HW/HW3.py").read_text(), {})
+
 
 
 
